d5c2db4223ebcdadfa5858cd1977a877baa0196a
Michael Hunter <elec.mike@gmail.com>
2021-01-15 11:51:54 +1100

    Bumped version to 0.0.15

--- dist-c/microvium.c
+++ dist-c/microvium.c
@@ -3,7 +3,7 @@
 /*
  * Microvium Bytecode Interpreter
  *
- * Version: 0.0.14
+ * Version: 0.0.15
  *
  * This file contains the Microvium virtual machine C implementation.
  *
--- test/getting-started/code/microvium/microvium.h
+++ test/getting-started/code/microvium/microvium.h
@@ -3,7 +3,7 @@
 /*
  * Microvium Bytecode Interpreter
  *
- * Version: 0.0.14
+ * Version: 0.0.15
  *
  * This is the main header for the Microvium bytecode interpreter. Latest source
  * available at https://microvium.com. Raise issues at
