bf7bb0ea32c9223326b04c3f82196499050eff2a
Michael Hunter <elec.mike@gmail.com>
2021-08-04 07:09:13 +1000

    Integrated stringify-structured to stringify the analysis

--- lib/src-to-il/analyze-scopes/stringify-analysis.ts
+++ lib/src-to-il/analyze-scopes/stringify-analysis.ts
@@ -1,96 +1,96 @@
-import { assertUnreachable, defineFormat as withFormat, notUndefined, stringifyGeneric, stringifyIdentifier, stringifyStringLiteral, unexpected } from '../../utils';
+import { assertUnreachable, notUndefined, stringifyIdentifier, stringifyStringLiteral, unexpected } from '../../utils';
 import { Binding, BlockScope, FunctionScope, ModuleScope, Scope, AnalysisModel, Slot } from '.';
-import { FunctionLikeScope } from './analysis-model';
+import { block, inline, list, Stringifiable, stringify, text } from 'stringify-structured';
 
-export function stringifyAnalysis(scopeInfo: AnalysisModel) {
-  return `${
-    `[this module slot] ${scopeInfo.thisModuleSlot.name}`
-  }\n${
-    [...scopeInfo.freeVariables]
-      .map(v => `[free var] ${stringifyIdentifier(v)}`)
-      .join('\n')
-  }\n${
-    scopeInfo.moduleImports
-      .map(v => `[import slot] ${v.slot.name} [from ${stringifyStringLiteral(v.source)}]`)
-      .join('\n')
-  }\n${
-    scopeInfo.exportedBindings
-      .map(v => `[export binding] ${v.name} [in slot] ${stringifySlot(v.slot)}`)
-      .join('\n')
-  }\n${
-    scopeInfo.globalSlots
-      .map(g => `[global slot] ${g.name}`)
-      .join('\n')
-  }\n${
-    stringifyScope(scopeInfo.moduleScope, '')
-  }`
+export function stringifyAnalysis(analysis: AnalysisModel) {
+  return stringify(renderAnalysis(analysis));
 }
 
-function stringifyScope(scope: Scope, indent: string): string {
+function renderAnalysis(analysis: AnalysisModel): Stringifiable {
+  return list('; ', [
+    { key: '[this module slot]', value: stringifyStringLiteral(analysis.thisModuleSlot.name) },
+
+    ...[...analysis.freeVariables]
+      .map(v => inline`[free var] ${v}`),
+
+    ...analysis.moduleImports
+      .map(v => inline`[import slot] ${v.slot.name} [from] ${v.source}`),
+
+    ...analysis.exportedBindings
+      .map(v => inline`[export binding] ${v.name} [in slot] ${renderSlot(v.slot)}`),
+
+    ...analysis.globalSlots
+      .map(g => inline`[global slot] ${g.name}`),
+
+    renderScope(analysis.moduleScope)
+  ])
+}
+
+function renderScope(scope: Scope): Stringifiable {
   switch (scope.type) {
-    case 'ModuleScope': return stringifyModuleScope(scope, indent);
-    case 'FunctionScope': return stringifyFunctionScope(scope, indent);
-    case 'BlockScope': return stringifyBlockScope(scope, indent);
+    case 'ModuleScope': return renderModuleScope(scope);
+    case 'FunctionScope': return renderFunctionScope(scope);
+    case 'BlockScope': return renderBlockScope(scope);
     default: return assertUnreachable(scope);
   }
 }
 
-function stringifyModuleScope(scope: ModuleScope, indent: string) {
-  return `${indent}module ${stringifyGeneric({
+function renderModuleScope(scope: ModuleScope): Stringifiable {
+  return inline`module ${{
     ilFunctionId: scope.ilFunctionId,
     functionIsClosure: scope.functionIsClosure,
     varDeclarations: scope.varDeclarations,
     prologue: scope.prologue,
-    closureSlots: scope.closureSlots?.map(s => withFormat(stringifySlot(s))),
-    children: scope.children.map(c => withFormat(indent => stringifyScope(c, indent))),
-  }, indent)}`
+    closureSlots: scope.closureSlots?.map(renderSlot),
+    children: scope.children.map(c => renderScope(c)),
+  }}`
 }
 
-function stringifyFunctionScope(scope: FunctionScope, indent: string) {
-  return `${indent}${
-    scope.functionIsClosure ? 'closure ' : ''
+function renderFunctionScope(scope: FunctionScope): Stringifiable {
+  return inline`${
+    text`${scope.functionIsClosure ? 'closure ' : ''}`
   }function ${
-    scope.funcName ?? '<anonymous>'
-  } {${
-    scope.closureSlots
-      ? `\n${indent}[closure scope ${notUndefined(scope.closureSlots.length)} slots]`
-      : ''
+    text`${scope.funcName ?? '<anonymous>'}`
+  } ${
+    block`{${
+      scope.closureSlots
+        ? text`[closure scope ${notUndefined(scope.closureSlots.length)} slots]`
+        : ''
+    }}`
   }${
-    stringifyScopeVariables(scope, indent)
-  }\n${indent}}`
+    renderScopeVariables(scope)
+  }}`
 }
 
-function stringifyBlockScope(scope: BlockScope, indent: string) {
-  return `${indent}block {${
-    stringifyScopeVariables(scope, indent)
-  }\n${indent}}`
+function renderBlockScope(scope: BlockScope): Stringifiable {
+  return block`block {${
+    renderScopeVariables(scope)
+  }}`
 }
 
-function stringifyScopeVariables(scope: Scope, indent: string) {
-  return Object.entries(scope.bindings)
-    .map(([k, v]) => `\n${indent}${stringifyBinding(k, v, indent)}`)
-    .join('')
+function renderScopeVariables(scope: Scope): Stringifiable {
+  return list('; ', Object.values(scope.bindings).map(renderBinding))
 }
 
-function stringifyBinding(name: string, binding: Binding, indent: string) {
-  let s = `${indent}${binding.kind} ${stringifyIdentifier(binding.name)}`;
+function renderBinding(binding: Binding): Stringifiable {
+  let s = `${binding.kind} ${stringifyIdentifier(binding.name)}`;
   if (binding.isDeclaredReadonly) s = `readonly ${s}`;
   if (binding.isWrittenTo) s = `writable ${s}`;
   if (binding.slot?.type === 'GlobalSlot') s = `global ${s}`;
   if (binding.slot?.type === 'ClosureSlot') s = `closure ${s}`;
   if (binding.slot?.type === 'LocalSlot') s = `local ${s}`;
   if (binding.isExported) s = `export ${s}`;
-  return s;
+  return text`${s}`;
 }
 
-function stringifySlot(slot?: Slot): string {
-  if (!slot) return '<no slot>';
+function renderSlot(slot?: Slot): Stringifiable {
+  if (!slot) return text`<no slot>`;
   switch (slot.type) {
-    case 'ArgumentSlot': return `[arg slot] ${slot.argIndex}`;
-    case 'ClosureSlot': return `[closure slot] ${slot.index}`;
-    case 'GlobalSlot': return `[global slot] ${slot.name}`;
-    case 'LocalSlot': return `[local slot] ${slot.index}`;
-    case 'ModuleImportExportSlot': return `[import/export slot] ${slot.propertyName} [in] ${stringifySlot(slot.moduleNamespaceObjectSlot)}`;
+    case 'ArgumentSlot': return inline`[arg slot] ${slot.argIndex}`;
+    case 'ClosureSlot': return inline`[closure slot] ${slot.index}`;
+    case 'GlobalSlot': return inline`[global slot] ${slot.name}`;
+    case 'LocalSlot': return inline`[local slot] ${slot.index}`;
+    case 'ModuleImportExportSlot': return inline`[import/export slot] ${slot.propertyName} [in] ${renderSlot(slot.moduleNamespaceObjectSlot)}`;
     default: return assertUnreachable(slot);
   }
 }
