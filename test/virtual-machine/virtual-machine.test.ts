ea2d798d33400e225a96dce4fee1f431d426a5ca
Michael Hunter <elec.mike@gmail.com>
2021-08-13 11:20:55 +1000

    VirtualMachine tests passing

--- test/virtual-machine/virtual-machine.test.ts
+++ test/virtual-machine/virtual-machine.test.ts
@@ -10,7 +10,7 @@ import { virtualMachineTestFilenames as virtualMachineTestFilenames } from "./fi
 import Microvium from "../../lib";
 import { encodeSnapshot } from "../../lib/encode-snapshot";
 
-suite.skip(VirtualMachine.name, function () {
+suite(VirtualMachine.name, function () {
   test('hello-world', () => {
     const testResults = new TestResults();
     const outputFilenames = virtualMachineTestFilenames['hello-world'];
