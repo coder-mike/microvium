d600d9bace94f1f905201c3551bf7e1d47f4afc7
Michael Hunter <elec.mike@gmail.com>
2021-08-16 07:42:01 +1000

    Another test case for closures in loops

--- test/end-to-end/tests/closures-in-loops.test.mvm.js
+++ test/end-to-end/tests/closures-in-loops.test.mvm.js
@@ -11,15 +11,37 @@ expectedPrintout: |
   7, 7
   8, 8
   9, 9
+  1, 0
+  3, 2
+  5, 4
+  7, 6
+  9, 8
 ---*/
 
 vmExport(0, run);
 
 function run() {
+  test1();
+  test2();
+}
+
+function test1() {
   const arr = [];
   for (let x = 0; x < 10; x++) {
     const y = x;
     arr.push(() => print(`${x}, ${y}`));
+  }
+  for (let i = 0; i < arr.length; i++) {
+    arr[i]();
+  }
+}
+
+function test2() {
+  const arr = [];
+  for (let x = 0; x < 10; x++) {
+    const y = x;
+    arr.push(() => print(`${x}, ${y}`));
+    x++;
   }
   for (let i = 0; i < arr.length; i++) {
     arr[i]();
